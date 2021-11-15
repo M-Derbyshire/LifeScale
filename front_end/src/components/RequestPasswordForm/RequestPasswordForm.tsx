484920087d32c8427ac43429925073b5481d5abb
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-11-15 13:30:05 +0000

    Added a heading to RequestPasswordForm

--- front_end/src/components/RequestPasswordForm/RequestPasswordForm.tsx
+++ front_end/src/components/RequestPasswordForm/RequestPasswordForm.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import './RequestPasswordForm.scss';
 
 interface IRequestPasswordFormProps {
 	email:string;
@@ -15,11 +16,14 @@ const RequestPasswordForm:FC<IRequestPasswordFormProps> = (props) => {
 	
 	return (
 		<div className="RequestPasswordForm">
+			
+			<h1>Request New Password</h1>
+			
 			<form onSubmit={props.onSubmit}>
 				<label htmlFor={emailElemId}>Email: </label>
 				<input type="email" value={props.email} onChange={(e)=>props.setEmail(e.target.value)} />
-				
-				<input type="submit" value="Request Password" />
+				<br />
+				<input type="submit" value="Request New Password" />
 			</form>
 		</div>
 	);
