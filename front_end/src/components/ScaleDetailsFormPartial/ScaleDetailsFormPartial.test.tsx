0931683d2eee84110e37a92b2a14ec08a4fbe26e
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-11-20 15:10:40 +0000

    ScaleDetailsFormPartial will now display and set the state for dayCount, via the dayCount and setDayCount props (the dayCount cannot be a decimal, and has to be positive)

--- front_end/src/components/ScaleDetailsFormPartial/ScaleDetailsFormPartial.test.tsx
+++ front_end/src/components/ScaleDetailsFormPartial/ScaleDetailsFormPartial.test.tsx
@@ -42,3 +42,91 @@ test("ScaleDetailsFormPartial will call the setName callback when changing the n
 	expect(mockSetState).toHaveBeenCalledWith(newVal);
 	
 });
+
+
+
+
+test.each([
+	["1"],
+	["2"]
+])("ScaleDetailsFormPartial renders the dayCount prop as the dayCount", (countText) => {
+	
+	const { container } = render(<ScaleDetailsFormPartial 
+		name="test" 
+		setName={dummySetState} 
+		usesTimespans={true}
+		setUsesTimespans={dummySetState}
+		dayCount={countText}
+		setDayCount={dummySetState} />);
+	
+	const countInput = container.querySelector(".scaleDayCountInput");
+	
+	expect(countInput).not.toBeNull();
+	expect(countInput.value).toEqual(countText);
+	
+});
+
+test("ScaleDetailsFormPartial will use the dayCount prop as the dayCount onChange event", () => {
+	
+	const mockCB = jest.fn();
+	const newVal = 2;
+	
+	const { container } = render(<ScaleDetailsFormPartial 
+		name="test" 
+		setName={dummySetState} 
+		usesTimespans={true}
+		setUsesTimespans={dummySetState}
+		dayCount={1}
+		setDayCount={mockCB} />);
+	
+	const countInput = container.querySelector(".scaleDayCountInput");
+	
+	fireEvent.change(countInput, { target: { value: newVal } });
+	
+	expect(mockCB).toHaveBeenCalledWith(newVal);
+});
+
+test("ScaleDetailsFormPartial will not allow dayCount to be a negative number (and will set the state to 0 instead)", () => {
+	
+	const mockCB = jest.fn();
+	const newVal = -1;
+	
+	const { container } = render(<ScaleDetailsFormPartial 
+		name="test" 
+		setName={dummySetState} 
+		usesTimespans={true}
+		setUsesTimespans={dummySetState}
+		dayCount={1}
+		setDayCount={mockCB} />);
+	
+	const countInput = container.querySelector(".scaleDayCountInput");
+	
+	fireEvent.change(countInput, { target: { value: newVal } });
+	
+	expect(mockCB).toHaveBeenCalledWith(0);
+	expect(mockCB).not.toHaveBeenCalledWith(newVal);
+});
+
+test.each([
+	[1.2, 1],
+	[1.5, 2],
+	[1.7, 2]
+])("ScaleDetailsFormPartial will not allow dayCount to be a decimal number (will round the number instead)", (newVal, expectedVal) => {
+	
+	const mockCB = jest.fn();
+	
+	const { container } = render(<ScaleDetailsFormPartial 
+		name="test" 
+		setName={dummySetState} 
+		usesTimespans={true}
+		setUsesTimespans={dummySetState}
+		dayCount={1}
+		setDayCount={mockCB} />);
+	
+	const countInput = container.querySelector(".scaleDayCountInput");
+	
+	fireEvent.change(countInput, { target: { value: newVal } });
+	
+	expect(mockCB).not.toHaveBeenCalledWith(newVal);
+	expect(mockCB).toHaveBeenCalledWith(expectedVal);
+});
--- front_end/src/components/ScaleDetailsFormPartial/ScaleDetailsFormPartial.tsx
+++ front_end/src/components/ScaleDetailsFormPartial/ScaleDetailsFormPartial.tsx
@@ -27,6 +27,17 @@ const ScaleDetailsFormPartial:FC<IScaleDetailsFormPartialProps> = (props) => {
 			</label>
 			<br/>
 			
+			<label>
+				Scale Day Count: <input 
+					type="number" 
+					className="scaleDayCountInput" 
+					min="0" 
+					step="1"
+					value={props.dayCount} 
+					onChange={(e)=>props.setDayCount( (Number(e.target.value) < 0) ? 0 : Math.round(Number(e.target.value)) )} />
+			</label>
+			<br/>
+			
 		</div>
 	);
 	
