92d92ebbcc78b1ac338e709d0a7edc89f0c1fe8a
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-11-20 14:40:11 +0000

    Added explanatory documentaing comment to AddItemCard

--- front_end/src/components/AddItemCard/AddItemCard.tsx
+++ front_end/src/components/AddItemCard/AddItemCard.tsx
@@ -5,6 +5,10 @@ interface IAddItemCardProps {
 	onClick:()=>void;
 }
 
+/*
+	Card to be an item in a flex container. This card will be used to take you to 
+	A screen to add a new item
+*/
 const AddItemCard:FC<IAddItemCardProps> = (props) => {
 	
 	return (
