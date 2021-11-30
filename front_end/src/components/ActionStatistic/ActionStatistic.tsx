5abc2c864eda1406155fa4b2da95c0856ed6e62f
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-11-30 19:57:56 +0000

    added explanatory comments to ActionStatistic

--- front_end/src/components/ActionStatistic/ActionStatistic.tsx
+++ front_end/src/components/ActionStatistic/ActionStatistic.tsx
@@ -6,7 +6,10 @@ interface IActionStatisticProps {
 	percentage:number;
 }
 
-
+/*
+	Used to display percentage information for actions that the user has recorded (say, what 
+	percentage of events on a scale are of the action type in question)
+*/
 const ActionStatistic:FC<IActionStatisticProps> = (props) => {
 	
 	return (
