27a079facc094088f1bbcef3551b9339f3208c76
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-12-01 22:51:19 +0000

    wrote explanatory comments for IScaleItem

--- front_end/src/components/ScaleBalanceDisplay/ScaleBalanceDisplay.tsx
+++ front_end/src/components/ScaleBalanceDisplay/ScaleBalanceDisplay.tsx
@@ -1,10 +1,13 @@
 import React, { Component, ReactElement } from 'react';
 import './ScaleBalanceDisplay.scss';
 
+//ScaleItems represent an item (say, a category) to be placed on the scale
 interface IScaleItem {
-	label:string;
-	weight:number;
-	color:string;
+	label:string; //The name or title of the scale
+	weight:number; //A number to represent the weight. 
+					//This could be a percentage, a fraction, an int (as long as the type of number used is 
+					//consistent for every item)
+	color:string; //CSS color to be used for the item
 }
 
 interface IScaleBalanceDisplayProps {
