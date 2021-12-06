ca36fc36b07e10c1f6d9c91883445503cd9b4811
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-12-06 12:20:47 +0000

    TimespanFormPartial will now display and set the minutes, via the props, and will not allow the mins to be decimals, or negative

--- front_end/src/components/TimespanFormPartial/TimespanFormPartial.test.tsx
+++ front_end/src/components/TimespanFormPartial/TimespanFormPartial.test.tsx
@@ -1,3 +1,71 @@
 import TimespanFormPartial from './TimespanFormPartial';
 import { render, fireEvent } from '@testing-library/react';
 
+const emptySetState = (x)=>{};
+
+test.each([
+	[120],
+	[240],
+	[350],
+	[90]
+])("TimespanFormPartial will display the given minute count", (mins) => {
+	
+	const { container } = render(<TimespanFormPartial minutes={mins} setMinutes={emptySetState} />);
+	
+	const minuteInput = container.querySelector(".timespanMinuteInput");
+	
+	expect(minuteInput.value).toEqual(mins.toString());
+	
+});
+
+test.each([
+	[120],
+	[240],
+	[350],
+	[90]
+])("TimespanFormPartial will call the setMinutes prop with the minute value, when setting the minute input", (newMins) => {
+	
+	const mockSetState = jest.fn();
+	
+	const { container } = render(<TimespanFormPartial minutes={0} setMinutes={mockSetState} />);
+	
+	const minuteInput = container.querySelector(".timespanMinuteInput");
+	
+	fireEvent.change(minuteInput, { target: { value: newMins } });
+	
+	expect(mockSetState).toHaveBeenCalledWith(newMins);
+	
+});
+
+test("TimespanFormPartial will not allow minutes to be a negative number (and will set the state to 0 instead)", () => {
+	
+	const mockCB = jest.fn();
+	const newVal = -1;
+	
+	const { container } = render(<TimespanFormPartial minutes={1} setMinutes={mockCB} />);
+	
+	const minuteInput = container.querySelector(".timespanMinuteInput");
+	
+	fireEvent.change(minuteInput, { target: { value: newVal } });
+	
+	expect(mockCB).toHaveBeenCalledWith(0);
+	expect(mockCB).not.toHaveBeenCalledWith(newVal);
+});
+
+test.each([
+	[1.2, 1],
+	[1.5, 2],
+	[1.7, 2]
+])("TimespanFormPartial will not allow minutes to be a decimal number (will round the number instead)", (newVal, expectedVal) => {
+	
+	const mockCB = jest.fn();
+	
+	const { container } = render(<TimespanFormPartial minutes={1} setMinutes={mockCB} />);
+	
+	const minuteInput = container.querySelector(".timespanMinuteInput");
+	
+	fireEvent.change(minuteInput, { target: { value: newVal } });
+	
+	expect(mockCB).not.toHaveBeenCalledWith(newVal);
+	expect(mockCB).toHaveBeenCalledWith(expectedVal);
+});
--- front_end/src/components/TimespanFormPartial/TimespanFormPartial.tsx
+++ front_end/src/components/TimespanFormPartial/TimespanFormPartial.tsx
@@ -15,6 +15,17 @@ const TimespanFormPartial:FC<ITimespanFormPartialProps> = (props) => {
 	return (
 		<div className="TimespanFormPartial">
 			
+			<label>
+				Time spent (in minutes): <input 
+					type="number"
+					className="timespanMinuteInput"
+					step="1"
+					min="0"
+					max="1440" //Total minutes in a day
+					value={props.minutes}
+					onChange={(e) => props.setMinutes( (Number(e.target.value) < 0) ? 0 : Math.round(Number(e.target.value)) )} />
+			</label>
+			
 		</div>
 	);
 	
