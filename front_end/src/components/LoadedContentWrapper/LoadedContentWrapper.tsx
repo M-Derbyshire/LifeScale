ab40124a792831063a7f5b24bc6c41de0b5e7181
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-12-08 12:00:42 +0000

    Added explanatory comment to LoadedContentWrapper

--- front_end/src/components/LoadedContentWrapper/LoadedContentWrapper.tsx
+++ front_end/src/components/LoadedContentWrapper/LoadedContentWrapper.tsx
@@ -7,6 +7,14 @@ interface ILoadedContentWrapperProps {
 }
 
 
+/*
+	Used when you want to render something, but may not have all the data needed to render it yet.
+	
+	This will render a loading message or icon (this may change thoughout the life of the project), if 
+	nothing is provided in the "render" prop.
+	
+	If the errorMessage prop is provided, that will be rendered instead
+*/
 const LoadedContentWrapper:FC<ILoadedContentWrapperProps> = (props) => {
 	
 	
