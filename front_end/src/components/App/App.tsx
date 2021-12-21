b3326b4ccf7fc53d02a597d1be1e434b1e512f90
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-12-21 10:22:39 +0000

    ChangePasswordForm is now ChangePasswordFormPartial

--- front_end/src/components/App/App.tsx
+++ front_end/src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import UserDetailsForm from '../UserDetailsForm/UserDetailsForm';
-import ChangePasswordForm from '../ChangePasswordForm/ChangePasswordForm';
+import ChangePasswordFormPartial from '../ChangePasswordFormPartial/ChangePasswordFormPartial';
 
 function App() {
 	
@@ -15,10 +15,10 @@ function App() {
 				user={ {id:"test", email:"test@test.com", password:"test", forename:"test", surname: "test"} }
 				setUser={dummyEmpty} onSubmit={dummySubmit} submitButtonText="Save" headingText="Edit User"
 				passwordForm={
-					<ChangePasswordForm 
+					<ChangePasswordFormPartial 
 						currentPassword="test" newPassword="test" newPasswordIsConfirmed={true} 
 						setCurrentPassword={dummyEmpty} setNewPassword={dummyEmpty} 
-						setNewPasswordIsConfirmed={dummyEmpty} onSubmit={dummySubmit} />
+						setNewPasswordIsConfirmed={dummyEmpty} />
 				} />
 		</div>
 	);
