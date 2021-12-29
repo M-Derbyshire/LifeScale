7f7752f74e1b55a066c765fdd6e254394c1e4676
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-12-29 16:20:53 +0000

    Changed some styling (by adding a container for the flex items) to RecordActionForm, as the heading was being inlcuded in the flex

--- front_end/src/components/RecordActionForm/RecordActionForm.tsx
+++ front_end/src/components/RecordActionForm/RecordActionForm.tsx
@@ -54,49 +54,51 @@ export default class RecordActionForm extends Component<IRecordActionFormProps>
 					<h2>Record Action</h2>
 				</header>
 				
-				<form onSubmit={this.props.onSubmit}>
-					
-					<label>
-						Category: <select className="categorySelect" 
-										value={this.props.selectedCategoryID} 
-										onChange={(e) => this.props.setSelectedCategoryID(e.target.value)}>
-							
-							{this.props.categories.map(
-								(cat) => this.mapItemToOptionElem(cat.name, cat.id)
-							)}
-							
-						</select>
-					</label>
-					
-					
-					<label>
-						Action: <select className="actionSelect" 
-									value={this.props.selectedActionID} 
-									onChange={(e) => this.props.setSelectedActionID(e.target.value)}>
-							
-							{this.props.categories.find(cat => cat.id === this.props.selectedCategoryID)!.actions.map(
-								(act) => this.mapItemToOptionElem(act.name, act.id)
-							)}
-							
-						</select>
-					</label>
-					
-					
-					<label>
-						Date: <input 
-								type="date" 
-								className="actionDate" 
-								value={this.convertDateToString(this.props.timespan.date)}
-								onChange={(e) => this.props.setTimespan({ ...this.props.timespan, date: new Date(e.target.value) })} />
-					</label>
-					
-					{this.props.usesTimespans && <TimespanFormPartial 
-						minutes={this.props.timespan.minuteCount}
-						setMinutes={(mins:number) => this.props.setTimespan({...this.props.timespan, minuteCount: mins})} />}
-					
-					<input type="submit" value="Record Action" />
-					
-				</form>
+				<div className="formContainer">
+					<form onSubmit={this.props.onSubmit}>
+						
+						<label>
+							Category: <select className="categorySelect" 
+											value={this.props.selectedCategoryID} 
+											onChange={(e) => this.props.setSelectedCategoryID(e.target.value)}>
+								
+								{this.props.categories.map(
+									(cat) => this.mapItemToOptionElem(cat.name, cat.id)
+								)}
+								
+							</select>
+						</label>
+						
+						
+						<label>
+							Action: <select className="actionSelect" 
+										value={this.props.selectedActionID} 
+										onChange={(e) => this.props.setSelectedActionID(e.target.value)}>
+								
+								{this.props.categories.find(cat => cat.id === this.props.selectedCategoryID)!.actions.map(
+									(act) => this.mapItemToOptionElem(act.name, act.id)
+								)}
+								
+							</select>
+						</label>
+						
+						
+						<label>
+							Date: <input 
+									type="date" 
+									className="actionDate" 
+									value={this.convertDateToString(this.props.timespan.date)}
+									onChange={(e) => this.props.setTimespan({ ...this.props.timespan, date: new Date(e.target.value) })} />
+						</label>
+						
+						{this.props.usesTimespans && <TimespanFormPartial 
+							minutes={this.props.timespan.minuteCount}
+							setMinutes={(mins:number) => this.props.setTimespan({...this.props.timespan, minuteCount: mins})} />}
+						
+						<input type="submit" value="Record Action" />
+						
+					</form>
+				</div>
 			</div>
 		);
 	}
