ba95787f896911eaaa3fd5de54d5edbc596657fc
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-12-29 21:24:37 +0000

    Setup the basic outline of ActionHistoryItem, and defined the props

---
+++ front_end/src/components/ActionHistoryItem/ActionHistoryItem.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import './ActionHistoryItem.scss';
+import ITimespan from '../../interfaces/ITimespan';
+
+interface IActionHistoryItemProps {
+	categoryName:string;
+	actionName:string;
+	timeSpan:ITimespan;
+	usesTimespans:boolean;
+}
+
+
+const ActionHistoryItem:FC<IActionHistoryItemProps> = (props) => {
+	
+	
+	return (
+		<div className="ActionHistoryItem">
+			
+		</div>
+	);
+	
+};
+
+
+export default ActionHistoryItem;
