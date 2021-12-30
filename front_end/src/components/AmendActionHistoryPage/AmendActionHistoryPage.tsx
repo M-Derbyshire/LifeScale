8028bc8ef57be9ee9b10fa1c1615f715fd36ff2b
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-12-30 12:07:46 +0000

    setup the outlne of AmendActionHistoryPage, and defiend the props

---
+++ front_end/src/components/AmendActionHistoryPage/AmendActionHistoryPage.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './AmendActionHistoryPage.scss';
+import ITimespan from '../../interfaces/ITimespan';
+
+
+interface IActionHistoryItem {
+	categoryName:string;
+	actionName:string;
+	timespan:ITimespan;
+	deleteHandler:()=>void;
+}
+
+interface IAmendActionHistoryPageProps {
+	scaleName:string;
+	items:IActionHistoryItem[];
+}
+
+
+
+/*
+	Used to delete previous occurences of actions on a given scale, and to add new ones.
+	(Once RecordActionFormContainer has been completed, the recording functionality can be added to this)
+*/
+export default class AmendActionHistoryPage extends Component<IAmendActionHistoryPageProps> {
+	
+	
+	
+	render()
+	{
+		
+		
+		return (
+			<div className="AmendActionHistoryPage">
+				
+			</div>
+		);
+	}
+	
+};
+
+export type { IActionHistoryItem };
