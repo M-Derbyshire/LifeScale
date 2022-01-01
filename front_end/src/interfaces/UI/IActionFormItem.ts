50c0a448cd7868d10e87efe5f806a05b0e890ae4
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-01 13:08:54 +0000

    Setup IActionFormItem

---
+++ front_end/src/interfaces/UI/IActionFormItem.ts
@@ -0,0 +1,13 @@
+
+//Used to represent a single action's required form info/callbacks
+interface IActionFormItem {
+	name:string;
+	setName:(name:string)=>void;
+	weight:number;
+	setWeight:(weight:number)=>void;
+	onSubmit:()=>void;
+	onDelete?:()=>void;
+}
+
+
+export default IActionFormItem;
