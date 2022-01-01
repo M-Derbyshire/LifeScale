c9af5b7586df63c5d6adf9bbd40ef7301fb026bd
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-01 13:24:08 +0000

    setup the outline of ActionsForm, and defined the props

---
+++ front_end/src/components/ActionsForm/ActionsForm.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './ActionsForm.scss';
+import IActionFormItem from '../../interfaces/UI/IActionFormItem';
+
+
+interface IActionsFormProps {
+	actions:IActionFormItem[];
+	newAction:IActionFormItem;
+}
+
+interface IActionsFormState {
+	displayNewActionForm:boolean;
+}
+
+export default class ActionsForm extends Component<IActionsFormProps, IActionsFormState> {
+	
+	
+	render()
+	{
+		
+		return (
+			<div className="ActionsForm">
+				
+			</div>
+		);
+	}
+	
+};
