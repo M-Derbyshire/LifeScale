8cd1865c2ddba966455673ab7e5f2847404c9774
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-07 08:48:36 +0000

    Styled the borders/colors of CardDisplay, and also the empty text display

--- front_end/src/components/App/App.tsx
+++ front_end/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import CategoryDetailsForm from '../CategoryDetailsForm/CategoryDetailsForm';
+import CardDisplay from '../CardDisplay/CardDisplay';
 
 function App() {
 	
@@ -9,55 +9,8 @@ function App() {
 	
 	return (
 		<div className="App">
-			<CategoryDetailsForm 
-				categoryItem={{
-					name: "Test category",
-					setName: dummyEmpty,
-					color: "Red",
-					setColor: dummyEmpty,
-					desiredWeight: 1,
-					setDesiredWeight: dummyEmpty,
-					
-					newAction: {
-						name: "test new act",
-						setName: dummyEmpty,
-						weight: 1,
-						setWeight: dummyEmpty,
-						onSubmit: dummySubmit,
-						onDelete: dummySubmit,
-						badSaveErrorMessage: "test bad save",
-						goodSaveMessage: "test good save"
-					},
-					actions: [{
-						name: "test act 1",
-						setName: dummyEmpty,
-						weight: 1,
-						setWeight: dummyEmpty,
-						onSubmit: dummySubmit,
-						onDelete: dummySubmit,
-						badSaveErrorMessage: "test bad save",
-						goodSaveMessage: "test good save"
-					}, {
-						name: "test act 2",
-						setName: dummyEmpty,
-						weight: 1,
-						setWeight: dummyEmpty,
-						onSubmit: dummySubmit,
-						onDelete: dummySubmit,
-						badSaveErrorMessage: "test bad save",
-						goodSaveMessage: "test good save"
-					}],
-					
-					onSubmit: dummySubmit,
-					onDelete: dummySubmit,
-					badSaveErrorMessage: "Test bad save",
-					goodSaveMessage: "Test good save"
-				}}
-	
-				headingText={"test header"}
-				
-				backButtonHandler={dummySubmit}
-				disableSubmit={true} />
+			<CardDisplay emptyDisplayMessage="There are no categories created for this scale.">
+			</CardDisplay>
 		</div>
 	);
 }
