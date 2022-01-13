ce3f4832f0c490b8afbdb3cd841f968949c2b2a2
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-13 15:45:33 +0000

    PercentageStatistic now displays it's children

--- front_end/src/components/PercentageStatistic/PercentageStatistic.test.tsx
+++ front_end/src/components/PercentageStatistic/PercentageStatistic.test.tsx
@@ -1,12 +1,18 @@
 import PercentageStatistic from './PercentageStatistic';
 import { render, screen } from '@testing-library/react';
 
+const dummyStatistic = {
+	label: "test",
+	percentage: 1,
+	id: "test"
+}
+
 test.each([
 	["test1"],
 	["test2"]
-])("PercentageStatistic will display the given label text prop", (labelText) => {
+])("PercentageStatistic will display the given label text", (labelText) => {
 	
-	render(<PercentageStatistic statistic={{ label: labelText, percentage: 1 }} />)
+	render(<PercentageStatistic statistic={{ ...dummyStatistic, label: labelText }} />)
 	
 	const labelElem = screen.getByText(labelText + ":");
 	
@@ -17,12 +23,31 @@ test.each([
 test.each([
 	[10],
 	[20]
-])("PercentageStatistic will display the given percentage prop", (percentage) => {
+])("PercentageStatistic will display the given percentage", (percentage) => {
 	
-	render(<PercentageStatistic statistic={{ label: "test", percentage }} />)
+	render(<PercentageStatistic statistic={{ ...dummyStatistic, percentage }} />)
 	
 	const percentElem = screen.getByText(percentage.toString() + "%");
 	
 	expect(percentElem).not.toBeNull();
+	
+});
+
+test("PercentageStatistic will display the given children in a list of PercentageStatistics", () => {
+	
+	const children = [
+		{ label: "testChild1", percentage: 11, id: "testChild1" },
+		{ label: "testChild2", percentage: 12, id: "testChild2" }
+	];
+	
+	const { container } = render(<PercentageStatistic statistic={{ ...dummyStatistic, children }} />)
+	
+	const childElems = container.querySelectorAll(".PercentageStatistic li .PercentageStatistic");
+	
+	expect(childElems.length).toBe(children.length);
+	childElems.forEach((elem, idx) => {
+		expect(elem.textContent).toEqual(expect.stringContaining(children[idx].label));
+		expect(elem.textContent).toEqual(expect.stringContaining(children[idx].percentage.toString()));
+	});
 	
 });
--- front_end/src/components/PercentageStatistic/PercentageStatistic.tsx
+++ front_end/src/components/PercentageStatistic/PercentageStatistic.tsx
@@ -1,10 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, ReactElement } from 'react';
 import './PercentageStatistic.scss';
 import IPercentageStatistic from '../../interfaces/UI/IPercentageStatistic';
 
 interface IPercentageStatisticProps {
 	statistic: IPercentageStatistic;
 }
+
+
+const mapChildToLi = (child:IPercentageStatistic):ReactElement => (<li key={child.id}>
+	<PercentageStatistic statistic={child} />
+</li>);
+
+
 
 /*
 	Used to display percentage information for actions that the user has recorded (say, what 
@@ -16,6 +23,10 @@ const PercentageStatistic:FC<IPercentageStatisticProps> = (props) => {
 		<div className="PercentageStatistic">
 			<span className="statisticLabel">{props.statistic.label}: </span>
 			<span className="statisticPercentage">{props.statistic.percentage}%</span>
+			
+			<ul className="statisticChildren">
+				{props.statistic.children && props.statistic.children.map(mapChildToLi)}
+			</ul>
 		</div>
 	);
 	
