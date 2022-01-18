149932a4e2c7fd7608bd8bb30ec282655ed2ae1f
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-18 12:29:48 +0000

    Added explanatory opening comment to DropdownContentBar

--- front_end/src/components/DropdownContentBar/DropdownContentBar.tsx
+++ front_end/src/components/DropdownContentBar/DropdownContentBar.tsx
@@ -6,7 +6,9 @@ interface IDropdownContentBarProps {
 	children?: ReactChild | ReactChild[];
 }
 
-
+/*
+	Displays a bar that will expand, as a drop-down, to display its children
+*/
 const DropdownContentBar:FC<IDropdownContentBarProps> = (props) => {
 	
 	const barExpanded = useState(false);
