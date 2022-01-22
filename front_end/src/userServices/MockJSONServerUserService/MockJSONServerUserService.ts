738c5fe3448dbc1bfa4cd152d32fcc2cbf4bb228
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-22 13:24:20 +0000

    setup the outline of the json server user service

---
+++ front_end/src/userServices/MockJSONServerUserService/MockJSONServerUserService.ts
@@ -0,0 +1,142 @@
+import IUserService from '../../interfaces/api_access/IUserService';
+import IUser from '../../interfaces/IUser';
+import IScale from '../../interfaces/IScale';
+import ICategory from '../../interfaces/ICategory';
+import IAction from '../../interfaces/IAction';
+import ITimespan from '../../interfaces/ITimespan';
+
+export default class MockJSONServerUserService implements IUserService {
+	
+	//Private properties not available for the JS version we're transpiling to
+	_apiURLBase:string;
+	
+	constructor(apiProtocol:string, apiDomain:string, apiPort:string)
+	{
+		this._apiURLBase="";
+	}
+	
+	loginUser(email:string, password:string)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	isLoggedIn()
+	{
+		throw new Error("Method not implemented");
+		return false;
+	}
+	
+	
+	
+	
+	
+	getLoadedUser()
+	{
+		throw new Error("Method not implemented");
+		return {
+			id:"test", 
+			email:"test@test.com",
+			forename:"test", 
+			surname: "test",
+			scales: []
+		}
+	}
+	
+	createUser(newUser: IUser & { password:string })
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	updateLoadedUser(newUserData:IUser)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	updateLoadedUserPassword(currentPassword:string, newPassword:string)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	
+	
+	
+	createScale(newScale:IScale)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	updateScale(currentScale:IScale, newScaleData:IScale)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	deleteScale(scale:IScale)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	
+	
+	
+	createCategory(newCategory:ICategory)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	updateCategory(currentCategory:ICategory, newCategoryData:ICategory)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	deleteCategory(category:ICategory)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	
+	
+	
+	createAction(newAction:IAction)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	updateAction(currentAction:IAction, newActionData:IAction)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	deleteAction(action:IAction)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	
+	
+	
+	createTimespan(newTimespan:ITimespan)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+	deleteTimespan(timespan:ITimespan)
+	{
+		throw new Error("Method not implemented");
+		return new Promise(()=>{});
+	}
+	
+}
