a1e76f50d932fdbbacd25c3278cb6e36de0bb0e5
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-26 11:21:42 +0000

    LoginPage had a test that weasn't very thorough (made sure the LoginForm was rendered, but didn't check the props). Corrected this

--- front_end/src/components/LoginPage/LoginPage.test.tsx
+++ front_end/src/components/LoginPage/LoginPage.test.tsx
@@ -4,66 +4,50 @@ import { MemoryRouter as Router } from 'react-router-dom';
 
 const dummySetState = (x)=>{};
 
-test("LoginPage will render a LoginForm", () => {
+test("LoginPage will render a LoginForm with the given props", () => {
+	
+	const initialEmailValue = "test@test.com";
+	const newEmailValue = "test2@test.com";
+	const initialPasswordValue = "test";
+	const newPasswordValue = "test2";
+	const mockSetEmail = jest.fn();
+	const mockSetPassword = jest.fn();
+	const mockLoginHandler = jest.fn();
+	const registerPath = "/testRegister";
+	const forgotPath = "/testNewPassword";
 	
 	const { container } = render(<Router>
 									<LoginPage 
-										email="email@email.com"
-										password="test"
-										setEmail={dummySetState}
-										setPassword={dummySetState}
-										loginHandler={()=>{}}
-										registerUserLinkPath={"/test"}
-										forgotPasswordLinkPath={"/test"} />
+										email={initialEmailValue}
+										password={initialPasswordValue}
+										setEmail={mockSetEmail}
+										setPassword={mockSetPassword}
+										loginHandler={mockLoginHandler}
+										registerUserLinkPath={registerPath}
+										forgotPasswordLinkPath={forgotPath} />
 								</Router>);
 	
 	const loginForm = container.querySelector(".LoginForm");
-	
 	expect(loginForm).not.toBeNull();
 	
-});
-
-test.each([
-	["/test1"],
-	["/test2"]
-])("LoginPage will render a link to go to the register route with the given register path prop", (testPath) => {
+	const emailInput = screen.getByDisplayValue(initialEmailValue);
+	console.log(emailInput.value);
+	fireEvent.change(emailInput, { target: { value: newEmailValue } });
+	expect(mockSetEmail).toHaveBeenCalledWith(newEmailValue);
 	
-	render(<Router>
-			<LoginPage 
-				email="email@email.com"
-				password="test"
-				setEmail={dummySetState}
-				setPassword={dummySetState}
-				loginHandler={()=>{}}
-				registerUserLinkPath={testPath}
-				forgotPasswordLinkPath={"/test"} />
-			</Router>);
+	const passwordInput = screen.getByDisplayValue(initialPasswordValue);
+	fireEvent.change(passwordInput, { target: { value: newPasswordValue } });
+	expect(mockSetPassword).toHaveBeenCalledWith(newPasswordValue);
+	
+	const form = container.querySelector(".LoginForm form");
+	fireEvent.submit(form);
+	expect(mockLoginHandler).toHaveBeenCalled();
 	
 	const registerLink = screen.getByText("register", {exact: false});
-	
-	expect(registerLink).toHaveAttribute("href", testPath);
-	
-});
-
-test.each([
-	["/test1"],
-	["/test2"]
-])("LoginPage will render a link to go to the forgot password route with the given path prop", (testPath) => {
-	
-	render(<Router>
-			<LoginPage 
-				email="email@email.com"
-				password="test"
-				setEmail={dummySetState}
-				setPassword={dummySetState}
-				loginHandler={()=>{}}
-				registerUserLinkPath={"/test"}
-				forgotPasswordLinkPath={testPath} />
-			</Router>);
+	expect(registerLink).toHaveAttribute("href", registerPath);
 	
 	const forgotLink = screen.getByText("forgot", {exact: false});
-	
-	expect(forgotLink).toHaveAttribute("href", testPath);
+	expect(forgotLink).toHaveAttribute("href", forgotPath);
 	
 });
 
