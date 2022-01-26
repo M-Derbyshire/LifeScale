6bb0840404609438b1c21b0b071c286bbcae4afe
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-26 14:22:48 +0000

    created the TestingDummyUserService, for use in logic container tests

---
+++ front_end/src/userServices/TestingDummyUserService/TestingDummyUserService.tsx
@@ -0,0 +1,150 @@
+import IUserService from '../../interfaces/api_access/IUserService';
+import IUser from '../../interfaces/IUser';
+import IScale from '../../interfaces/IScale';
+import ICategory from '../../interfaces/ICategory';
+import IAction from '../../interfaces/IAction';
+import ITimespan from '../../interfaces/ITimespan';
+
+
+
+// This implementation of IUserService is here for use in tests.
+// Tests can create an instance of this, then overwrite the required
+// methods with mocks.
+export default class TestingDummyUserService implements IUserService {
+	
+	_dummyUser:IUser = {
+		id: "mock-user",
+		email: "mock@user.com",
+		forename: "mock",
+		surname: "user",
+		scales: []
+	};
+	
+	
+	loginUser(email:string, password:string)
+	{
+		return new Promise((resolve, reject) => {
+			resolve(this._dummyUser);
+		});
+	}
+	
+	logoutUser()
+	{
+		return new Promise((resolve, reject) => {
+			resolve(null);
+		});
+	}
+	
+	isLoggedIn()
+	{
+		return false;
+	}
+	
+	requestNewPassword(email:string)
+	{
+		return new Promise((resolve, reject) => {
+			resolve(null);
+		});
+	}
+	
+	
+	
+	getLoadedUser()
+	{
+		return this._dummyUser;
+	}
+	
+	createUser(newUser: Omit<IUser, "id"> & { password:string })
+	{
+		return new Promise((resolve, reject) => {
+			resolve(this._dummyUser);
+		});
+	}
+	
+	updateLoadedUser(newUserData:IUser)
+	{
+		return new Promise((resolve, reject) => {
+			resolve(this._dummyUser);
+		});
+	}
+	
+	updateLoadedUserPassword(currentPassword:string, newPassword:string)
+	{
+		return new Promise((resolve, reject) => {
+			resolve(this._dummyUser);
+		});
+	}
+	
+	
+	
+	createScale(newScale:Omit<IScale, "id">) { 
+		return new Promise((resolve, reject) => {
+			resolve(newScale);
+		});
+	}
+	
+	updateScale(currentScale:IScale, newScaleData:IScale) {
+		return new Promise((resolve, reject) => {
+			resolve(newScaleData);
+		});
+	}
+	
+	deleteScale(scale:IScale) {
+		return new Promise((resolve, reject) => {
+			resolve(this._dummyUser.scales);
+		});
+	}
+	
+	
+	createCategory(parentScale:IScale, newCategory:Omit<ICategory, "id">) {
+		return new Promise((resolve, reject) => {
+			resolve(newCategory);
+		});
+	}
+	
+	updateCategory(currentCategory:ICategory, newCategoryData:ICategory) {
+		return new Promise((resolve, reject) => {
+			resolve(newCategoryData);
+		});
+	}
+	
+	deleteCategory(parentScale:IScale, category:ICategory) {
+		return new Promise((resolve, reject) => {
+			resolve(parentScale.categories);
+		});
+	}
+	
+	
+	createAction(parentCategory:ICategory, newAction:Omit<IAction, "id">) {
+		return new Promise((resolve, reject) => {
+			resolve(newAction);
+		});
+	}
+	
+	updateAction(currentAction:IAction, newActionData:IAction) {
+		return new Promise((resolve, reject) => {
+			resolve(newActionData);
+		});
+	}
+	
+	deleteAction(parentCategory:ICategory, action:IAction) {
+		return new Promise((resolve, reject) => {
+			resolve(parentCategory.actions);
+		});
+	}
+	
+	
+	createTimespan(parentAction:IAction, newTimespan:Omit<ITimespan, "id">) {
+		return new Promise((resolve, reject) => {
+			resolve(newTimespan);
+		});
+	}
+	
+	deleteTimespan(parentAction:IAction, timespan:ITimespan) {
+		return new Promise((resolve, reject) => {
+			resolve(parentAction.timespans);
+		});
+	}
+	
+	
+}
