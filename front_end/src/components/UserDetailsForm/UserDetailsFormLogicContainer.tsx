e12b00ebdb751213d91db8ab24ff9af21b825104
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-01-29 15:12:05 +0000

    Implemented the logic container for UserDetailsForm

--- front_end/src/components/UserDetailsForm/UserDetailsFormLogicContainer.tsx
+++ front_end/src/components/UserDetailsForm/UserDetailsFormLogicContainer.tsx
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, ReactElement } from 'react';
 import UserDetailsForm from './UserDetailsForm';
+import PasswordFormPartial from '../PasswordFormPartial/PasswordFormPartial';
+import ChangePasswordFormLogicContainer from '../ChangePasswordForm/ChangePasswordFormLogicContainer';
 import IUserService from '../../interfaces/api_access/IUserService';
 import IUser from '../../interfaces/IUser';
 
@@ -12,10 +14,12 @@ interface IUserDetailsFormLogicContainerProps {
 
 interface IUserDetailsFormLogicContainerState {
 	user?:IUser|Omit<IUser, "id"> & { password:string };
+	passwordIsConfirmed:boolean;
 	userSavedPreviously:boolean; //Have any saves been completed (during this use), even if this is a new user
 	badLoadErrorMessage?:string;
 	badSaveErrorMessage?:string;
 	goodSaveMessage?:string;
+	disableSubmit:boolean;
 }
 
 
@@ -43,7 +47,8 @@ export default class UserDetailsFormLogicContainer
 		try 
 		{
 			if(!this.props.isNewUser)
-				user = this.props.userService.getLoadedUser();
+				//Spread, so we don't change the userService user
+				user = { ...this.props.userService.getLoadedUser() };
 		} 
 		catch (err:any) 
 		{
@@ -54,23 +59,136 @@ export default class UserDetailsFormLogicContainer
 		
 		this.state = {
 			user,
+			passwordIsConfirmed: false,
 			userSavedPreviously: false,
 			badLoadErrorMessage,
 			badSaveErrorMessage: undefined,
-			goodSaveMessage: undefined
+			goodSaveMessage: undefined,
+			disableSubmit: false
 		};
 		
 	}
 	
 	
+	
+	
+	onSubmit(isCreatingUser:boolean)
+	{
+		this.setState({ disableSubmit: true });
+		
+		
+		const onGoodSaveNewState = {
+			user: undefined,
+			userSavedPreviously: true,
+			badSaveErrorMessage: undefined,
+			goodSaveMessage: "The user details have now been saved.",
+			disableSubmit: false
+		};
+		
+		const onBadSaveNewState = {
+			goodSaveMessage: undefined,
+			disableSubmit: false
+		};
+		
+		
+		if(isCreatingUser)
+		{
+			if("password" in this.state.user! && this.state.passwordIsConfirmed)
+			{
+				const correctlyTypedUserData:Omit<IUser, "id"> & { password:string } = {
+					email: this.state.user!.email,
+					password: this.state.user!.password,
+					forename: this.state.user!.forename,
+					surname: this.state.user!.surname,
+					scales: this.state.user!.scales
+				};
+				
+				this.props.userService.createUser(correctlyTypedUserData)
+					.then(user => this.setState({ ...onGoodSaveNewState, user }))
+					.catch(err => this.setState({ ...onBadSaveNewState, badSaveErrorMessage: err.message }));
+			}
+			else
+			{
+				this.setState({ 
+					...onBadSaveNewState, 
+					badSaveErrorMessage: "The password confirmation does not match the given password." 
+				});
+			}
+		}
+		else
+		{
+			if("id" in this.state.user!)
+			{
+				const correctlyTypedUserData:IUser = {
+					id: this.state.user!.id,
+					email: this.state.user!.email,
+					forename: this.state.user!.forename,
+					surname: this.state.user!.surname,
+					scales: this.state.user!.scales
+				};
+				
+				this.props.userService.updateLoadedUser(correctlyTypedUserData)
+					.then(user => this.setState({ ...onGoodSaveNewState, user }))
+					.catch(err => this.setState({ ...onBadSaveNewState, badSaveErrorMessage: err.message }));
+			}
+		}
+	}
+	
+	
+	
+	
 	render()
 	{
-		const isCreatingUser = (this.props.isNewUser && this.state.userSavedPreviously);
-		const rejectAccess = (!isCreatingUser && !this.props.userService.isLoggedIn());
+		const isCreatingUser = (this.props.isNewUser && !this.state.userSavedPreviously);
+		const rejectAccess = (
+			!isCreatingUser && !this.state.userSavedPreviously && !this.props.userService.isLoggedIn());
+		
+		
+		
+		
+		
+		let passwordForm:ReactElement|undefined = undefined; //undefined if editing user that is not logged in
+		if(isCreatingUser)
+			passwordForm = (<PasswordFormPartial 
+								password={("password" in this.state.user!) ? this.state.user!.password : ""}
+								setPassword={(password) => this.setState({
+									 user: { ...this.state.user!, password } 
+								})}
+								setPasswordIsConfirmed={(isConfirmed) => this.setState({
+									passwordIsConfirmed: isConfirmed
+								})} />);
+		else if (!this.props.isNewUser)
+			passwordForm = (<ChangePasswordFormLogicContainer userService={this.props.userService} />);
+		
+		
+		
+		
+		
+		const correctlyTypedUserData:IUser = {
+			id: ("id" in this.state.user!) ? this.state.user!.id : "",
+			email: this.state.user!.email,
+			forename: this.state.user!.forename,
+			surname: this.state.user!.surname,
+			scales: this.state.user!.scales
+		};
+		
+		
+		
 		
 		return (
 			<div className="UserDetailsFormLogicContainer">
-				
+				<UserDetailsForm 
+					user={correctlyTypedUserData}
+					setUser={(newUserDetails) => this.setState({ user: newUserDetails })}
+					headingText={(isCreatingUser) ? "Register" : "Update User Details"}
+					badLoadErrorMessage={this.state.badLoadErrorMessage}
+					passwordForm={passwordForm}
+					backButtonHandler={this.props.backButtonHandler}
+					onSubmit={() => this.onSubmit(isCreatingUser)}
+					disableSubmit={this.state.disableSubmit}
+					submitButtonText={(isCreatingUser) ? "Register" : "Save"}
+					badSaveErrorMessage={this.state.badSaveErrorMessage}
+					goodSaveMessage={this.state.goodSaveMessage} />
 			</div>
 		);
 	}
---
+++ front_end/src/components/UserDetailsForm/logicContainer.test.tsx
@@ -0,0 +1,445 @@
+import UserDetailsFormLogicContainer from './UserDetailsFormLogicContainer';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import TestingDummyUserService from '../../userServices/TestingDummyUserService/TestingDummyUserService';
+
+const dummyBackHandler = ()=>{};
+
+test("UserDetailsFormLogicContainer will render a UserDetailsForm", () => {
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={new TestingDummyUserService()}
+									isNewUser={true}
+									backButtonHandler={dummyBackHandler} />);
+	
+	expect(container.querySelector(".UserDetailsForm")).not.toBeNull();
+	
+});
+
+test("UserDetailsFormLogicContainer will handle the state and submission of UserDetailsForm when creating", () => {
+	
+	const userToCreate = {
+		email: "test@test.com",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	//The below 2 are not included in the above object, as will only be there at different times
+	const userToCreateId = "jashashsdh";
+	const userToCreatePassword = "testpass";
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.createUser = jest.fn().mockResolvedValue({ ...userToCreate, id: userToCreateId });
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={true}
+									backButtonHandler={dummyBackHandler} />);
+	
+	const emailInput = container.querySelector("input[type=email]");
+	fireEvent.change(emailInput, { target: { value: userToCreate.email } });
+	
+	const forenameInput = container.querySelector(".userForenameInput");
+	fireEvent.change(forenameInput, { target: { value: userToCreate.forename } });
+	
+	const surnameInput = container.querySelector(".userSurnameInput");
+	fireEvent.change(surnameInput, { target: { value: userToCreate.surname } });
+	
+	const passwordInputs = container.querySelectorAll("input[type=password]");
+	passwordInputs.forEach(input => fireEvent.change(input, { target: { value: userToCreatePassword } }));
+	
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	expect(mockUserService.createUser).toHaveBeenCalledWith({ ...userToCreate, password: userToCreatePassword });
+	
+});
+
+test("UserDetailsFormLogicContainer will not create user if the password is not confirmed", () => {
+	
+	const userToCreate = {
+		email: "test@test.com",
+		password: "testpass",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.createUser = jest.fn();
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={true}
+									backButtonHandler={dummyBackHandler} />);
+	
+	const emailInput = container.querySelector("input[type=email]");
+	fireEvent.change(emailInput, { target: { value: userToCreate.email } });
+	
+	const forenameInput = container.querySelector(".userForenameInput");
+	fireEvent.change(forenameInput, { target: { value: userToCreate.forename } });
+	
+	const surnameInput = container.querySelector(".userSurnameInput");
+	fireEvent.change(surnameInput, { target: { value: userToCreate.surname } });
+	
+	
+	//Set one password to be incorrect
+	const passwordInputs = container.querySelectorAll("input[type=password]");
+	expect(passwordInputs.length).toBe(2);
+	fireEvent.change(passwordInputs[0], { target: { value: userToCreate.password } });
+	fireEvent.change(passwordInputs[1], { target: { value: userToCreate.password + "1234" } });
+	
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	expect(mockUserService.createUser).not.toHaveBeenCalled();
+	
+});
+
+test("UserDetailsFormLogicContainer will handle the state and submission of UserDetailsForm when editing", async () => {
+	
+	const userToLoad = {
+		email: "test@test.com",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	const userNewValues = {
+		email: "test2@test.com",
+		forename: "test2",
+		surname: "testing2",
+		scales: []
+	};
+	
+	//The below is not included in the above object, as will only be there at different times
+	const userToLoadId = "jashashsdh";
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.getLoadedUser = () => { return { ...userToLoad, id: userToLoadId }; };
+	mockUserService.updateLoadedUser = jest.fn().mockResolvedValue({ ...userNewValues, id: userToLoadId });
+	
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={false}
+									backButtonHandler={dummyBackHandler} />);
+	
+	const emailInput = screen.getByDisplayValue(userToLoad.email);
+	fireEvent.change(emailInput, { target: { value: userNewValues.email } });
+	
+	const forenameInput = screen.getByDisplayValue(userToLoad.forename);
+	fireEvent.change(forenameInput, { target: { value: userNewValues.forename } });
+	
+	const surnameInput = screen.getByDisplayValue(userToLoad.surname);
+	fireEvent.change(surnameInput, { target: { value: userNewValues.surname } });
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	await waitFor(() => expect(mockUserService.updateLoadedUser)
+							.toHaveBeenCalledWith({ ...userNewValues, id: userToLoadId }));
+	
+});
+
+test("UserDetailsFormLogicContainer will render a ChangePasswordFormLogicContainer when editing loaded user, not a PasswordFormPartial", () => {
+	
+	const userToLoad = {
+		id: "fjshdfkjhafhs",
+		email: "test@test.com",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.getLoadedUser = () => userToLoad;
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={false}
+									backButtonHandler={dummyBackHandler} />);
+	
+	expect(container.querySelector(".ChangePasswordFormLogicContainer")).not.toBeNull();
+	expect(container.querySelector(".UserDetailsForm > .PasswordFormPartial")).toBeNull();
+	
+});
+
+test("UserDetailsFormLogicContainer will not render a ChangePasswordFormLogicContainer when creating a user, but instead a PasswordFormPartial", () => {
+	
+	let mockUserService = new TestingDummyUserService();
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={true}
+									backButtonHandler={dummyBackHandler} />);
+	
+	expect(container.querySelector(".ChangePasswordFormLogicContainer")).toBeNull();
+	expect(container.querySelector(".PasswordFormPartial")).not.toBeNull();
+	
+});
+
+
+test("UserDetailsFormLogicContainer will not render any password forms after creating a user", async () => {
+	
+	const userToCreate = {
+		email: "test@test.com",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	//The below 2 are not included in the above object, as will only be there at different times
+	const userToCreateId = "jashashsdh";
+	const userToCreatePassword = "testpass";
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.createUser = jest.fn().mockResolvedValue({ ...userToCreate, id: userToCreateId });
+	
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={true}
+									backButtonHandler={dummyBackHandler} />);
+	
+	const emailInput = container.querySelector("input[type=email]");
+	fireEvent.change(emailInput, { target: { value: userToCreate.email } });
+	
+	const forenameInput = container.querySelector(".userForenameInput");
+	fireEvent.change(forenameInput, { target: { value: userToCreate.forename } });
+	
+	const surnameInput = container.querySelector(".userSurnameInput");
+	fireEvent.change(surnameInput, { target: { value: userToCreate.surname } });
+	
+	const passwordInputs = container.querySelectorAll("input[type=password]");
+	passwordInputs.forEach(input => fireEvent.change(input, { target: { value: userToCreatePassword } }));
+	
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	await waitFor(() => {
+		expect(container.querySelector(".ChangePasswordFormLogicContainer")).toBeNull();
+		expect(container.querySelector(".PasswordFormPartial")).toBeNull();
+	});
+	
+});
+
+test("UserDetailsFormLogicContainer will pass any load errors to the UserDetailsForm", () => {
+	
+	const errorMessage = "test error on loading";
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.getLoadedUser = () => { throw new Error(errorMessage); };
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={false}
+									backButtonHandler={dummyBackHandler} />);
+	
+	const detailsForm = container.querySelector(".UserDetailsForm");
+	expect(detailsForm.textContent).toEqual(expect.stringContaining(errorMessage));
+	
+});
+
+test("UserDetailsFormLogicContainer will pass any on-create save errors to the UserDetailsForm", async () => {
+	
+	const errorMessage = "test error on saving";
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.createUser = jest.fn().mockRejectedValue(new Error(errorMessage));
+	
+	
+	const userToCreate = {
+		email: "test@test.com",
+		password: "test123",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={true}
+									backButtonHandler={dummyBackHandler} />);
+	
+	const emailInput = container.querySelector("input[type=email]");
+	fireEvent.change(emailInput, { target: { value: userToCreate.email } });
+	
+	const forenameInput = container.querySelector(".userForenameInput");
+	fireEvent.change(forenameInput, { target: { value: userToCreate.forename } });
+	
+	const surnameInput = container.querySelector(".userSurnameInput");
+	fireEvent.change(surnameInput, { target: { value: userToCreate.surname } });
+	
+	const passwordInputs = container.querySelectorAll("input[type=password]");
+	passwordInputs.forEach(input => fireEvent.change(input, { target: { value: userToCreate.password } }));
+	
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	
+	
+	await waitFor(() => {
+		const detailsForm = container.querySelector(".UserDetailsForm");
+		expect(detailsForm.textContent).toEqual(expect.stringContaining(errorMessage));
+	});
+	
+});
+
+test("UserDetailsFormLogicContainer will pass any on-update save errors to the UserDetailsForm", async () => {
+	
+	const errorMessage = "test error on updating";
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.updateLoadedUser = jest.fn().mockRejectedValue(new Error(errorMessage));
+	
+	
+	const userToCreate = {
+		email: "test@test.com",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={false}
+									backButtonHandler={dummyBackHandler} />);
+	
+	const forenameInput = container.querySelector(".userForenameInput");
+	fireEvent.change(forenameInput, { target: { value: "newValue" } });
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	
+	
+	await waitFor(() => {
+		const detailsForm = container.querySelector(".UserDetailsForm");
+		expect(detailsForm.textContent).toEqual(expect.stringContaining(errorMessage));
+	});
+	
+});
+
+
+test("UserDetailsFormLogicContainer will disable submit on UserDetailsForm, when creating, then re-enable", async () => {
+	
+	const userToCreate = {
+		email: "test@test.com",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	//The below 2 are not included in the above object, as will only be there at different times
+	const userToCreateId = "jashashsdh";
+	const userToCreatePassword = "testpass";
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.createUser = jest.fn().mockResolvedValue({ ...userToCreate, id: userToCreateId });
+	
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={true}
+									backButtonHandler={dummyBackHandler} />);
+	
+	
+	const submitButton = container.querySelector(".UserDetailsForm input[type=submit]");
+	
+	expect(submitButton).not.toBeDisabled(); //Is this not disabled
+	
+	//Create the user
+	const emailInput = container.querySelector("input[type=email]");
+	fireEvent.change(emailInput, { target: { value: userToCreate.email } });
+	
+	const forenameInput = container.querySelector(".userForenameInput");
+	fireEvent.change(forenameInput, { target: { value: userToCreate.forename } });
+	
+	const surnameInput = container.querySelector(".userSurnameInput");
+	fireEvent.change(surnameInput, { target: { value: userToCreate.surname } });
+	
+	const passwordInputs = container.querySelectorAll("input[type=password]");
+	passwordInputs.forEach(input => fireEvent.change(input, { target: { value: userToCreatePassword } }));
+	
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	
+	await waitFor(() => expect(submitButton).toBeDisabled()); //Is this disabled
+	
+	await waitFor(() => expect(submitButton).not.toBeDisabled()); //Is this re-enabled
+	
+});
+
+test("UserDetailsFormLogicContainer will disable submit on UserDetailsForm, when updating, then re-enable", async () => {
+	
+	const userToLoad = {
+		id: "dkjhashsdakjsh",
+		email: "test@test.com",
+		forename: "test",
+		surname: "testing",
+		scales: []
+	};
+	
+	let mockUserService = new TestingDummyUserService();
+	mockUserService.getLoadedUser = () => userToLoad;
+	mockUserService.updateLoadedUser = jest.fn().mockResolvedValue(userToLoad);
+	
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={mockUserService}
+									isNewUser={false}
+									backButtonHandler={dummyBackHandler} />);
+	
+	
+	const submitButton = container.querySelector(".UserDetailsForm input[type=submit]");
+	
+	expect(submitButton).not.toBeDisabled(); //Is this not disabled
+	
+	
+	const forenameInput = container.querySelector(".userForenameInput");
+	fireEvent.change(forenameInput, { target: { value: "newValue" } });
+	
+	//Submit form
+	const form = container.querySelector(".UserDetailsForm form");
+	fireEvent.submit(form);
+	
+	
+	await waitFor(() => expect(submitButton).toBeDisabled()); //Is this disabled
+	
+	await waitFor(() => expect(submitButton).not.toBeDisabled()); //Is this re-enabled
+	
+});
+
+
+test("UserDetailsFormLogicContainer will pass the backButtonHandler down to UserDetailsForm", () => {
+	
+	const mockBackHandler = jest.fn();
+	
+	const { container } = render(<UserDetailsFormLogicContainer
+									userService={new TestingDummyUserService()}
+									isNewUser={true}
+									backButtonHandler={mockBackHandler} />);
+	
+	const backButton = container.querySelector(".UserDetailsForm .userDetailsBackButton");
+	
+	fireEvent.click(backButton);
+	
+	expect(mockBackHandler).toHaveBeenCalled();
+	
+});
