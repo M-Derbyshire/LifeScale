0ce8f63b9732a4d6bc6d1cb4066a1dc2067face9
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-02-01 14:12:21 +0000

    Setup the outline of the logic container for RecordActionForm, and defined the props/state

---
+++ front_end/src/components/RecordActionForm/RecordActionFormLogicContainer.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import RecordActionForm from './RecordActionForm';
+import IUserService from '../../interfaces/api_access/IUserService';
+import IScale from '../../interfaces/IScale';
+
+
+interface IRecordActionFormLogicContainerProps {
+	userService:IUserService;
+	scale:IScale;
+}
+
+interface IRecordActionFormLogicContainerState {
+	selectedCategoryID:string;
+	selectedActionID:string;
+	
+	usesTimespans?:boolean;
+	timespan:ITimespan;
+	
+	badSaveErrorMessage?:string;
+	goodSaveMessage?:string;
+}
+
+
+export default class RecordActionFormLogicContainer
+	extends Component<IRecordActionFormLogicContainerProps, IRecordActionFormLogicContainerState>
+{
+	
+	constructor(props:IRecordActionFormLogicContainerProps)
+	{
+		super(props);
+		
+		
+	}
+	
+	
+	render()
+	{
+		
+		return (
+			<div className="RecordActionFormLogicContainer">
+				
+			</div>
+		);
+	}
+	
+}
