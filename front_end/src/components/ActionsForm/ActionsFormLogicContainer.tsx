aa730e3cf5293872fc11ce6dd91c246304b59f0a
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-02-07 20:30:18 +0000

    Logic container for ActionsForm now handles state/updates/deletes

--- front_end/src/components/ActionsForm/ActionsFormLogicContainer.tsx
+++ front_end/src/components/ActionsForm/ActionsFormLogicContainer.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ActionsForm from './ActionsForm';
 import IUserService from '../../interfaces/api_access/IUserService';
 import IAction from '../../interfaces/IAction';
+import ICategory from '../../interfaces/ICategory';
+import IActionFormItem from '../../interfaces/UI/IActionFormItem';
 
 
 
@@ -16,11 +18,13 @@ interface IActionsFormLogicContainerProps {
 	userService:IUserService;
 	scaleID:string;
 	categoryID:string;
+	onCategoryLoadError?:(errorMessage:string)=>void;
 }
 
 interface IActionsFormLogicContainerState {
-	actions?:IAction[];
-	loadingError?:string;
+	category?:ICategory;
+	actions?:IAction[]; //Actions that we can edit as state
+	originalActions?:IAction[]; //References to actions in userService (used when updating/deleting)
 	lastActionSaveMessage:IActionSaveMessage; //We only need to display the last save message (good or bad)
 }
 
@@ -29,24 +33,138 @@ export default class ActionsFormLogicContainer
 	extends Component<IActionsFormLogicContainerProps, IActionsFormLogicContainerState>
 {
 	
+	stdCategoryLoadError = "Unable to load the requested category.";
+	stdActionsLoadError = "Unable to load the actions for the requested category";
+	
+	
 	constructor(props:IActionsFormLogicContainerProps)
 	{
 		super(props)
 		
+		const category = this.props.userService.getCategory(this.props.categoryID, this.props.scaleID);
+		let actions:IAction[]|undefined = undefined;
+		let originalActions:IAction[]|undefined = undefined;
 		
+		if (!category) 
+		{
+			actions = [];
+			if(this.props.onCategoryLoadError)
+				this.props.onCategoryLoadError(this.stdCategoryLoadError);
+		}
+		else
+		{
+			try 
+			{
+				originalActions = category.actions;
+				actions = this.getRefreshedActionList(category);
+			} 
+			catch {}
+		}
+		
+		if (category && (!actions || !originalActions) && this.props.onCategoryLoadError)
+			this.props.onCategoryLoadError(this.stdActionsLoadError);
+		
+		
+		const lastActionSaveMessage = {
+			actionID: "",
+			isError: false,
+			saveMessage: ""
+		};
+		
+		
+		this.state = {
+			category,
+			actions,
+			originalActions,
+			lastActionSaveMessage
+		};
+	}
+	
+	
+	
+	getRefreshedActionList(category:ICategory)
+	{
+		try 
+		{
+			if(category)
+			{
+				return category.actions.map(action => ({ ...action }));
+			}
+		} 
+		catch {}
+		
+		return undefined;
+	}
+	
+	
+	updateSingleActionState(newActionData:IAction, newActionIndex:number)
+	{
+		this.setState(state => {
+			return { 
+				actions: state.actions!.map((action, i) => {
+					if(i === newActionIndex)
+						return newActionData;
+					else
+						return action;
+				})
+			};
+		});
+	}
+	
+	
+	
+	updateHandler(action:IAction, index:number)
+	{
+		if(this.state.originalActions)
+			this.props.userService.updateAction(this.state.originalActions[index], action)
+				.then(action => {})
+				.catch(err => {});
+	}
+	
+	deleteHandler(actionIndex:number)
+	{
+		if(this.state.category && this.state.originalActions)
+			this.props.userService.deleteAction(this.state.category, this.state.originalActions[actionIndex])
+				.then(actions => {})
+				.catch(err => {})
+	}
+	
+	
+	mapActionToFormItem(action:IAction, index:number, isNewAction = false):IActionFormItem
+	{
+		const lastActionSaveMessage = this.state.lastActionSaveMessage;
+		const hasSaveMessage = (lastActionSaveMessage.actionID === action.id);
+		const goodSaveMessage = 
+			(hasSaveMessage && !lastActionSaveMessage.isError) ? lastActionSaveMessage.saveMessage : undefined;
+		const badSaveMessage = 
+			(hasSaveMessage && lastActionSaveMessage.isError) ? lastActionSaveMessage.saveMessage : undefined;
+		
+		
+		return {
+			id: action.id,
+			name: action.name,
+			setName: (name:string) => this.updateSingleActionState({ ...action, name }, index),
+			weight: action.weight,
+			setWeight: (weight:number) => this.updateSingleActionState({ ...action, weight }, index),
+			onSubmit: () => this.updateHandler(action, index),
+			onDelete: (!isNewAction) ? () => this.deleteHandler(index) : undefined,
+			badSaveErrorMessage: undefined,
+			goodSaveMessage: undefined
+		};
 	}
 	
 	
 	
 	render()
 	{
-		
+		const actions = this.state.actions!.map((act, i) => this.mapActionToFormItem(act, i, false));
 		
 		return (
 			<div className="ActionsFormLogicContainer">
 				<ActionsForm 
-					actions={[]}
+					actions={actions}
 					newAction={{
+						id: "",
 						name: "test",
 						setName:(name:string)=>{},
 						weight: 1,
--- front_end/src/components/ActionsForm/logicContainer.test.tsx
+++ front_end/src/components/ActionsForm/logicContainer.test.tsx
@@ -1,14 +1,178 @@
 import ActionsFormLogicContainer from './ActionsFormLogicContainer';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen, within } from '@testing-library/react';
 import TestingDummyUserService from '../../userServices/TestingDummyUserService/TestingDummyUserService';
+
+const dummyCategory = {
+	id: "testcat",
+	name: "test-category",
+	color: "red",
+	desiredWeight: 1,
+	actions: [
+		{
+			id: "testact1",
+			name: "test-action-1",
+			weight: 1,
+			timespans: []
+		},
+		{
+			id: "testact2",
+			name: "test-action-2",
+			weight: 2,
+			timespans: []
+		},
+		{
+			id: "testact3",
+			name: "test-action-3",
+			weight: 3,
+			timespans: []
+		}
+	]
+};
+
+const dummyScale = {
+	id: "testscale",
+	name: "test-scale",
+	usesTimespans: false,
+	displayDayCount: 7,
+	categories: [dummyCategory]
+};
+
+
+const dummyUserService = new TestingDummyUserService();
+dummyUserService.getCategory = (catID, scaleID) => dummyCategory;
+
+
 
 test("ActionsFormLogicContainer will render a ActionsForm", () => {
 	
 	const { container } = render(<ActionsFormLogicContainer
-									userService={new TestingDummyUserService()}
-									scaleID="testScale"
-									categoryID="testcat" />)
+									userService={dummyUserService}
+									scaleID={dummyScale.id}
+									categoryID={dummyCategory.id} />)
 	
 	expect(container.querySelector(".ActionsForm")).not.toBeNull();
 	
 });
+
+
+test("ActionsFormLogicContainer will load the categories with the correct scale/category ids", () => {
+	
+	const mockUserService = { ...dummyUserService };
+	mockUserService.getCategory = jest.fn();
+	
+	const { container } = render(<ActionsFormLogicContainer
+									userService={mockUserService}
+									scaleID={dummyScale.id}
+									categoryID={dummyCategory.id} />)
+	
+	expect(mockUserService.getCategory).toHaveBeenCalledWith(dummyCategory.id, dummyScale.id);
+	
+});
+
+test("ActionsFormLogicContainer will load the category and display the actions as SingleActionForm", () => {
+	
+	const { container } = render(<ActionsFormLogicContainer
+									userService={dummyUserService}
+									scaleID={dummyScale.id}
+									categoryID={dummyCategory.id} />);
+	
+	const singleActionForms = container.querySelectorAll(".SingleActionForm");
+	expect(singleActionForms.length).toBe(dummyCategory.actions.length);
+	
+	dummyCategory.actions.forEach(
+		(act, i) => expect(within(singleActionForms[i]).getByDisplayValue(act.name)).not.toBeNull()
+	);
+	
+});
+
+
+test("ActionsFormLogicContainer will handle the state of the actions", () => {
+	
+	const newNames = dummyCategory.actions.map(act => act.name + "new");
+	const newWeights = dummyCategory.actions.map(act => act.weight + 10);
+	
+	const { container } = render(<ActionsFormLogicContainer
+									userService={dummyUserService}
+									scaleID={dummyScale.id}
+									categoryID={dummyCategory.id} />)
+	
+	
+	const singleActionForms = container.querySelectorAll(".SingleActionForm");
+	expect(singleActionForms.length).toBe(dummyCategory.actions.length);
+	
+	dummyCategory.actions.forEach((act, i) => {
+		const nameInput = within(singleActionForms[i]).getByDisplayValue(act.name);
+		const weightInput = within(singleActionForms[i]).getByDisplayValue(act.weight);
+		
+		fireEvent.change(nameInput, { target: { value: newNames[i] } });
+		expect(nameInput.value).toEqual(newNames[i]);
+		
+		fireEvent.change(weightInput, { target: { value: newWeights[i] } });
+		expect(Number(weightInput.value)).toEqual(newWeights[i]);
+	});
+	
+});
+
+
+test("ActionsFormLogicContainer will give the current actions delete handlers", () => {
+	
+	const mockUserService = { ...dummyUserService };
+	mockUserService.deleteAction = jest.fn().mockResolvedValue([]);
+	
+	const { container } = render(<ActionsFormLogicContainer
+									userService={mockUserService}
+									scaleID={dummyScale.id}
+									categoryID={dummyCategory.id} />)
+	
+	const deleteButtons = screen.getAllByRole("button", { name: /delete/i });
+	expect(deleteButtons.length).toBe(dummyCategory.actions.length);
+	
+	dummyCategory.actions.forEach((act, i) => {
+		fireEvent.click(deleteButtons[i]);
+		expect(mockUserService.deleteAction).toHaveBeenCalledWith(dummyCategory, dummyCategory.actions[i]);
+	});
+	
+});
+
+test("ActionsFormLogicContainer will give the current actions submit handlers", () => {
+	
+	const newName = "testChange";
+	
+	const mockUserService = { ...dummyUserService };
+	mockUserService.updateAction = jest.fn().mockResolvedValue(dummyCategory.actions[0]);
+	
+	const { container } = render(<ActionsFormLogicContainer
+									userService={mockUserService}
+									scaleID={dummyScale.id}
+									categoryID={dummyCategory.id} />)
+	
+	const forms = container.querySelectorAll(".SingleActionForm form");
+	expect(forms.length).toBe(dummyCategory.actions.length);
+	
+	forms.forEach((form, i) => {
+		const action = dummyCategory.actions[i];
+		
+		const nameInput = within(form).getByDisplayValue(action.name);
+		fireEvent.change(nameInput, { target: { value: newName } });
+		
+		fireEvent.submit(form);
+		expect(mockUserService.updateAction).toHaveBeenCalledWith(action, {
+			...action,
+			name: newName
+		});
+	});
+	
+});
+
+
+// refreshes list on delete
+
+// delete error message
+
+// save message on save
+
+// onCategoryLoadError prop
+// - for category load
+// - for accessing actions
+
+// new action stuff
