fc07048ba8a184e98b55da35f3704941ac78b0f5
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-02-09 18:55:03 +0000

    Wrote the outline of logic container for CategoryDetailsForm, and defined the props/state

---
+++ front_end/src/components/CategoryDetailsForm/CategoryDetailsFormLogicContainer.tsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import CategoryDetailsForm from './CategoryDetailsForm';
+import IUserService from '../../interfaces/api_access/IUserService';
+
+
+interface ICategoryDetailsFormLogicContainerProps {
+	scaleID:string;
+	categoryID?:string; //If undefined, this form will be used to create. Otherwise, form will update
+	userService:IUserService;
+	backButtonHandler:()=>void;
+};
+
+interface ICategoryDetailsFormLogicContainerState {
+	name:string;
+	color:string;
+	desiredWeight:number;
+	badSaveErrorMessage?:string;
+	goodSaveMessage?:string;
+};
+
+
+export default class CategoryDetailsFormLogicContainer
+	extends Component<ICategoryDetailsFormLogicContainerProps, ICategoryDetailsFormLogicContainerState>
+{
+	
+	constructor(props:ICategoryDetailsFormLogicContainerProps)
+	{
+		super(props)
+		
+		this.state = {
+			name: "",
+			color: "",
+			desiredWeight: 0
+		};
+	}
+	
+	
+	
+	render()
+	{
+		
+		
+		return (
+			<div className="CategoryDetailsFormLogicContainer">
+				
+			</div>
+		);
+	}
+	
+}
