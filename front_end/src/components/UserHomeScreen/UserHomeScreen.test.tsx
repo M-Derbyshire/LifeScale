17a88a0bda43fa98234396d277110391d1619ee0
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-02-24 19:24:36 +0000

    UserHomeScreen now displays an EmptyContentMessage if no scales exist for the user

--- front_end/src/components/UserHomeScreen/UserHomeScreen.test.tsx
+++ front_end/src/components/UserHomeScreen/UserHomeScreen.test.tsx
@@ -1,7 +1,150 @@
+import UserHomeScreen from './UserHomeScreen';
+import { render, screen, waitFor } from '@testing-library/react';
+import TestingDummyUserService from '../../userServices/TestingDummyUserService/TestingDummyUserService';
+import IAction from '../../interfaces/IAction';
+
+
+const dummyCallbackNoParam = () => {};
+
+const dummyCategory1 = {
+	id: "testCat23132948284",
+	name: "testcat1",
+	color: "",
+	desiredWeight: 1,
+	actions: new Array<IAction>()
+};
+
+const dummyCategory2 = {
+	id: "testCatd3482384092348",
+	name: "testcat2",
+	color: "",
+	desiredWeight: 1,
+	actions: new Array<IAction>()
+};
+
+const dummyCategory3 = {
+	id: "testCat32482934809438",
+	name: "testcat3",
+	color: "",
+	desiredWeight: 1,
+	actions: new Array<IAction>()
+}
+
+const dummyScale = {
+	id: "testScale38312772389",
+	name: "testscale",
+	usesTimespans: true,
+	displayDayCount: 7,
+	categories: [dummyCategory1, dummyCategory2, dummyCategory3]
+}
 
 
 
-// If user has no scales, UserHomeScreen will display an EmptyContentMessage, within a NavigatableContentWrapper
+
+const dummyDesiredBalanceItems = [
+    {
+        label: "test1",
+        color: "red",
+        weight: 1
+    },
+    {
+        label: "test2",
+        color: "blue",
+        weight: 2
+    },
+    {
+        label: "test3",
+        color: "green",
+        weight: 3
+    }
+];
+
+const dummyCurrentBalanceItems = [
+    {
+        label: "test1",
+        color: "red",
+        weight: 3
+    },
+    {
+        label: "test2",
+        color: "blue",
+        weight: 2
+    },
+    {
+        label: "test3",
+        color: "green",
+        weight: 1
+    }
+];
+
+const dummyStatistics = [
+    {
+        id: "test1",
+        label: "test1",
+        percentage: 60,
+        children: [
+            {
+                id: "act1",
+                label: "act1",
+                percentage: 40
+            },
+            {
+                id: "act2",
+                label: "act2",
+                percentage: 20
+            }
+        ]
+    },
+    {
+        id: "test2",
+        label: "test3",
+        percentage: 40,
+        children: [
+            {
+                id: "act3",
+                label: "act3",
+                percentage: 10
+            },
+            {
+                id: "act4",
+                label: "act4",
+                percentage: 30
+            }
+        ]
+    }
+];
+
+
+
+const defaultProps = {
+    userService: new TestingDummyUserService(),
+    scales: [ {...dummyScale, name: "scale1" }, {...dummyScale, name: "scale2" } ],
+    selectedScale: dummyScale,
+    scaleLoadingError: undefined,
+    onSuccessfulLogout: dummyCallbackNoParam,
+    editScaleCallback: dummyCallbackNoParam,
+    onSuccessfulTimespanSave: dummyCallbackNoParam,
+    amendHistoryCallback: dummyCallbackNoParam,
+    editUserURL: "/test/edit",
+    createScaleURL: "/test/create",
+    scaleURLBase: "test",
+    desiredBalanceItems: dummyDesiredBalanceItems,
+    currentBalanceItems: dummyCurrentBalanceItems,
+    statistics: dummyStatistics
+};
+
+
+
+
+test("If user has no scales, UserHomeScreen will display an EmptyContentMessage, within a NavigatableContentWrapper", () => {
+    
+    const { container } = render(<UserHomeScreen { ...defaultProps } scales={[]} />);
+    
+    const emptyMessage = container.querySelector(".NavigatableContentWrapper .EmptyContentMessage");
+    
+    expect(emptyMessage).not.toBeNull();
+    
+});
 
 
 
--- front_end/src/components/UserHomeScreen/UserHomeScreen.tsx
+++ front_end/src/components/UserHomeScreen/UserHomeScreen.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
+import './UserHomeScreen.scss';
 import IUserService from '../../interfaces/api_access/IUserService';
 import IScale from '../../interfaces/IScale';
 import IPercentageStatistic from '../../interfaces/UI/IPercentageStatistic';
 import IScaleBalanceItem from '../../interfaces/UI/IScaleBalanceItem';
-import './UserHomeScreen.scss';
+import NavigatableContentWrapper from '../NavigatableContentWrapper/NavigatableContentWrapper';
+import LoadedContentWrapper from '../LoadedContentWrapper/LoadedContentWrapper';
+import EmptyContentMessage from '../EmptyContentMessage/EmptyContentMessage';
 
 
 interface IUserHomeScreenProps {
     userService:IUserService;
+    scales:IScale[];
     selectedScale?:IScale;
-    scaleLoadingError:string;
+    scaleLoadingError?:string;
     
     onSuccessfulLogout:()=>void; //Called after successful logout
     editScaleCallback:()=>void; //Called by the Edit Scale button
@@ -31,7 +35,17 @@ const UserHomeScreen:FC<IUserHomeScreenProps> = (props) => {
     
     return (
         <div className='UserHomeScreen'>
-            
+            <NavigatableContentWrapper navigationBar={(<nav>test</nav>)} smallScreenWidthPixels={500}>
+                
+                <LoadedContentWrapper render={(
+                    
+                    <div>
+                        {props.scales.length === 0 && <EmptyContentMessage message="No scales have been created." />}
+                    </div>
+                    
+                )} />
+                
+            </NavigatableContentWrapper>
         </div>
     );
     
