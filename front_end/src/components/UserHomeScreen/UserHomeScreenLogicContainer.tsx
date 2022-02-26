f0bf43aea64e4b0bcdebd5115525fe2b44a6aff3
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-02-26 18:12:23 +0000

    Setup outline of UserHomeScreenLogicContainer, and defined the props

---
+++ front_end/src/components/UserHomeScreen/UserHomeScreenLogicContainer.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import IUserService from '../../interfaces/api_access/IUserService';
+import UserHomeScreen from './UserHomeScreen';
+
+
+interface IUserHomeScreenLogicContainerProps {
+    userService:IUserService;
+    scaleURLBase:string; //E.g. "scales" in "/scales/id1234"
+    
+    onSuccessfulLogout:()=>void; //Called after successful logout
+    editScaleCallback:()=>void; //Called by the Edit Scale button
+    amendHistoryCallback:()=>void; // The callback for the amend history button
+};
+
+interface IUserHomeScreenLogicContainerState {
+    
+};
+
+
+export default class UserHomeScreenLogicContainer
+    extends Component<IUserHomeScreenLogicContainerProps, IUserHomeScreenLogicContainerState>
+{
+    
+    constructor(props:IUserHomeScreenLogicContainerProps)
+    {
+        super(props);
+        
+        
+    }
+    
+    
+    render()
+    {
+        
+        
+        return (
+            <div className="UserHomeScreenLogicContainer">
+                
+            </div>
+        );
+    }
+    
+}
