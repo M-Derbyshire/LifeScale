d40972dde3c1dcf02c05967d4facde5b65bedb20
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-02-27 14:29:52 +0000

    Logic container for UserHomeScreen will now load the selectedScale (or pass an error prop), and pass it on to the UserHomeScreen, along with the full scales list

--- front_end/src/components/UserHomeScreen/UserHomeScreenLogicContainer.tsx
+++ front_end/src/components/UserHomeScreen/UserHomeScreenLogicContainer.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import IUserService from '../../interfaces/api_access/IUserService';
+import IScale from '../../interfaces/IScale';
 import UserHomeScreen from './UserHomeScreen';
 
 
@@ -7,6 +8,8 @@ interface IUserHomeScreenLogicContainerProps {
     userService:IUserService;
     selectedScaleID?:string;
     scaleURLBase:string; //E.g. "scales" in "/scales/id1234"
+    editUserURL:string;
+    createScaleURL:string; 
     
     onSuccessfulLogout:()=>void; //Called after successful logout
     editScaleCallback:(scaleID:string)=>void; //Called by the Edit Scale button
@@ -14,7 +17,9 @@ interface IUserHomeScreenLogicContainerProps {
 };
 
 interface IUserHomeScreenLogicContainerState {
-    
+    scales:IScale[];
+    selectedScale?:IScale;
+    scaleLoadingError:string;
 };
 
 
@@ -22,11 +27,40 @@ export default class UserHomeScreenLogicContainer
     extends Component<IUserHomeScreenLogicContainerProps, IUserHomeScreenLogicContainerState>
 {
     
+    stdScaleLoadErrorMessage = "Unable to load the selected scale.";
+    
     constructor(props:IUserHomeScreenLogicContainerProps)
     {
         super(props);
         
+        let scaleLoadingError;
+        let scales:IScale[] = [];
+        let selectedScale;
         
+        try
+        {
+            scales = this.props.userService.getLoadedUser().scales;
+        }
+        catch (err:any)
+        {
+            scaleLoadingError = err.message;
+        }
+        
+        
+        if(this.props.selectedScaleID && !scaleLoadingError)
+        {
+            selectedScale = this.props.userService.getScale(this.props.selectedScaleID);
+            if(!selectedScale)
+                scaleLoadingError = this.stdScaleLoadErrorMessage;
+        }
+        
+        
+        
+        this.state = {
+            scaleLoadingError,
+            scales,
+            selectedScale,
+        };
     }
     
     
@@ -36,7 +70,24 @@ export default class UserHomeScreenLogicContainer
         
         return (
             <div className="UserHomeScreenLogicContainer">
-                
+                <UserHomeScreen 
+                    userService={this.props.userService}
+                    scales={this.state.scales}
+                    selectedScale={this.state.selectedScale}
+                    scaleLoadingError={this.state.scaleLoadingError}
+                    
+                    onSuccessfulLogout={this.props.onSuccessfulLogout}
+                    editScaleCallback={() => this.props.editScaleCallback("")}
+                    onSuccessfulTimespanSave={()=>{}}
+                    amendHistoryCallback={() => this.props.amendHistoryCallback("")}
+                    
+                    editUserURL={this.props.editUserURL}
+                    createScaleURL={this.props.createScaleURL}
+                    scaleURLBase={this.props.scaleURLBase}
+                    
+                    desiredBalanceItems={[]}
+                    currentBalanceItems={[]}
+                    statistics={[]} />
             </div>
         );
     }
--- front_end/src/components/UserHomeScreen/logicContainer.test.tsx
+++ front_end/src/components/UserHomeScreen/logicContainer.test.tsx
@@ -1,12 +1,134 @@
+import UserHomeScreenLogicContainer from './UserHomeScreenLogicContainer';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import TestingDummyUserService from '../../userServices/TestingDummyUserService/TestingDummyUserService';
 
 
-// UserHomeScreenLogicContainer will load the scales list, and pass down to UserHomeScreen
+const stdScaleLoadError = "Unable to load the selected scale.";
 
-// UserHomeScreenLogicContainer will load the selected scale with the given ID, and pass it down to UserHomeScreen
+const dummyCategory1 = {
+	id: "testCat23132948284",
+	name: "testcat1",
+	color: "",
+	desiredWeight: 1,
+	actions: [
+        {
+            id: "testAct2348743749874",
+            name: "testAct1",
+            weight: 1,
+            timespans: []
+        }
+    ]
+};
 
-// If not selected scale ID provided, UserHomeScreenLogicContainer will pass undefined down for selectedScale
+const dummyCategory2 = {
+	id: "testCatd3482384092348",
+	name: "testcat2",
+	color: "",
+	desiredWeight: 1,
+	actions: [
+        {
+            id: "testAct897432897497",
+            name: "testAct2",
+            weight: 1,
+            timespans: []
+        }
+    ]
+};
 
-// UserHomeScreenLogicContainer will pass down a scale loading error, if error loading scale
+const dummyCategory3 = {
+	id: "testCat32482934809438",
+	name: "testcat3",
+	color: "",
+	desiredWeight: 1,
+	actions: [
+        {
+            id: "testAct28438290842038",
+            name: "testAct3",
+            weight: 1,
+            timespans: []
+        }
+    ]
+}
+
+const dummyScale = {
+	id: "testScale",
+	name: "testscale",
+	usesTimespans: true,
+	displayDayCount: 7,
+	categories: [dummyCategory1, dummyCategory2, dummyCategory3]
+}
+
+const dummyScales = [ {...dummyScale, name: "scale1", id: "testScale1" }, {...dummyScale, name: "scale2", id: "testScale2" } ];
+
+const dummyUser = {
+    id: "testUser847274298734",
+    email: "test@test.com",
+	forename: "test",
+	surname: "test",
+	scales: dummyScales
+};
+
+
+
+const dummyCallbackNoParam = () => {};
+const dummyCallbackSingleParam = (x:any) => {};
+
+const dummyUserService = new TestingDummyUserService();
+dummyUserService.getLoadedUser = () => dummyUser;
+dummyUserService.getScale = (scaleID:string) => dummyUser.scales.filter(scale => scale.id === scaleID)[0];
+
+const defaultProps = {
+    userService: dummyUserService,
+    selectedScaleID: dummyUser.scales[0].id,
+    scaleURLBase: "test",
+    editUserURL: "/edit/user",
+    createScaleURL: "/scales/create",
+    onSuccessfulLogout: dummyCallbackNoParam,
+    editScaleCallback: dummyCallbackSingleParam,
+    amendHistoryCallback: dummyCallbackSingleParam,
+};
+
+
+
+
+
+test.each(dummyUser.scales.map(scale => [scale.id, scale.name]))
+    ("UserHomeScreenLogicContainer will load the selected scale with the given ID, and pass it down to UserHomeScreen", (scaleID, scaleName) => {
+    
+    const { container } = render(<Router><UserHomeScreenLogicContainer {...defaultProps} selectedScaleID={scaleID} /></Router>);
+    
+    const header = container.querySelector("header");
+    
+    expect(header.textContent).toEqual(expect.stringContaining(scaleName));
+    
+});
+
+test("UserHomeScreenLogicContainer will pass down a scale loading error, if error loading user", () => {
+    
+    const errorMessage = "Unable to load user, test error";
+    
+    const mockUserService = { ...dummyUserService };
+    mockUserService.getLoadedUser = () => { throw new Error(errorMessage) };
+    
+    const { container } = render(<Router><UserHomeScreenLogicContainer {...defaultProps} userService={mockUserService} /></Router>);
+    
+    expect(screen.getByText(errorMessage)).not.toBeNull();
+    
+});
+
+test("UserHomeScreenLogicContainer will pass down a scale loading error, if error loading scale", () => {
+    
+    const errorMessage = stdScaleLoadError;
+    
+    const mockUserService = { ...dummyUserService };
+    mockUserService.getScale = (scaleID:string) => undefined; // the return type for not found
+    
+    const { container } = render(<Router><UserHomeScreenLogicContainer {...defaultProps} userService={mockUserService} /></Router>);
+    
+    expect(screen.getByText(errorMessage)).not.toBeNull();
+    
+});
 
 
 
@@ -17,7 +139,7 @@
 
 
 
-
+// use test.each, with expected results
 // UserHomeScreenLogicContainer will calculate the statistics percentages for categories and actions, and pass them to UserHomeScreen
 
 // UserHomeScreenLogicContainer will calculate the desiredBalanceItems, and pass them to UserHomeScreen
