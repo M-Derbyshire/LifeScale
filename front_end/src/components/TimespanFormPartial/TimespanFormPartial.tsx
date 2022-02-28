f367b121957989d4ed7d67d9b11c7d26f7887638
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-02-28 12:10:52 +0000

    TimespanFormPartial had some issues with empty/invaid values in the number inputs. Replaced these with text inputs, tjat validate with regex, and properly handle these issues when updating the other (minute/hour) input as well

--- front_end/src/components/TimespanFormPartial/TimespanFormPartial.tsx
+++ front_end/src/components/TimespanFormPartial/TimespanFormPartial.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 interface ITimespanFormPartialProps {
 	minutes:number;
@@ -12,29 +12,59 @@ interface ITimespanFormPartialProps {
 */
 const TimespanFormPartial:FC<ITimespanFormPartialProps> = (props) => {
 	
+	
+	//Number type inputs can be an issue with react. Also tried external modules, but didn't work in the way we wanted
+	//Instead, will use a regex and validation on text inputs. Also, using the below state to allow for things such as 
+	//empty values in the fields (if input value is valid, the minute state is set. Otherwise, only the display value is set).
+	//The form should then fail to submit, if correctlly formatted data is not provided
+	const validNumberPattern = "[0-9]+\.?[0-9]{0,2}"; //up to 2 decimal places
+	const [minuteDisplayValue, setMinuteDisplayValue] = useState(props.minutes.toFixed(0));
+	const [hourDisplayValue, setHourDisplayValue] = useState((props.minutes / 60).toFixed(2));
+	
+	const customNumberValidationMessage = "Please provide a valid number";
+	
+	
 	return (
 		<div className="TimespanFormPartial">
 			
 			<label>
 				Time spent (in minutes): <input 
-					type="number"
+					type="text"
 					className="timespanMinuteInput"
-					step="1"
-					min="0"
-					max="1440" //Total minutes in a day
-					value={props.minutes}
-					onChange={(e) => props.setMinutes( (Number(e.target.value) < 0) ? 0 : Math.round(Number(e.target.value)) )} />
+					pattern={validNumberPattern}
+					value={minuteDisplayValue}
+					onChange={(e) => {
+						if(e.target.validity.valid)
+						{
+							const minutes = Number(e.target.value);
+							props.setMinutes(minutes);
+							setHourDisplayValue((minutes / 60).toFixed(2));
+						}
+						else
+							setHourDisplayValue("");
+						
+						setMinuteDisplayValue(e.target.value);
+					}} />
 			</label>
 			
 			<label>
 				Time spent (in hours): <input 
-					type="number"
+					type="text"
 					className="timespanHourInput"
-					step="0.01"
-					min="0"
-					max="24"
-					value={(props.minutes / 60).toFixed(2)}
-					onChange={(e) => props.setMinutes( (Number(e.target.value) < 0) ? 0 : Math.round(Number(e.target.value) * 60) )} />
+					pattern={validNumberPattern}
+					value={hourDisplayValue}
+					onChange={(e) => {
+						if(e.target.validity.valid)
+						{
+							const minutes = Number(e.target.value) * 60;
+							props.setMinutes(minutes);
+							setMinuteDisplayValue(minutes.toFixed(0));
+						}
+						else
+							setMinuteDisplayValue("");
+						
+						setHourDisplayValue(e.target.value);
+					}} />
 			</label>
 			
 		</div>
