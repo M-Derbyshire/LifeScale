2ca272facdf77b6497b50303748d4c8d00ed2c27
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-03-07 14:40:48 +0000

    typescript didn't like that I was adding a property to TestingDummyUserService in a mock, so added it

--- front_end/src/userServices/TestingDummyUserService/TestingDummyUserService.ts
+++ front_end/src/userServices/TestingDummyUserService/TestingDummyUserService.ts
@@ -21,6 +21,8 @@ export default class TestingDummyUserService implements IUserService {
 	};
 	
 	
+	mockLoggedInStatus:boolean = false;
+	
 	abortRequests() {}
 	
 	loginUser(email:string, password:string)
