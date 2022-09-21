a48643816f1778b1d18c1a564dfce4cfc9b50143
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-09-21 07:59:58 +0100

    inclued GORM created/updated/deleted fields in all models

--- back_end/go_gin/models/timespan.go
+++ back_end/go_gin/models/timespan.go
@@ -1,8 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Timespan struct {
+	gorm.Model
 	StrID       string    `gorm:"-" json:"id"` //The front end uses strings for the ID (incase this needs to use a NoSQL DB in the future)
 	ID          uint64    `gorm:"id" json:"-"`
 	Date        time.Time `gorm:"date" json:"date"`
--- back_end/go_gin/models/user.go
+++ back_end/go_gin/models/user.go
@@ -1,6 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
+	gorm.Model
 	StrID   string  `gorm:"-" json:"id"` //The front end uses strings for the ID (incase this needs to use a NoSQL DB in the future)
 	ID      uint64  `gorm:"id" json:"-"`
 	Email   string  `gorm:"email;unique" json:"email"`
