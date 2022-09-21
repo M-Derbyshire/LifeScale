7665366c85d7424712ff1d0e2e84f257c2523555
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-09-21 07:56:50 +0100

    set user email field to unique

--- back_end/go_gin/models/user.go
+++ back_end/go_gin/models/user.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	StrID   string  `gorm:"-" json:"id"` //The front end uses strings for the ID (incase this needs to use a NoSQL DB in the future)
 	ID      uint64  `gorm:"id" json:"-"`
-	Email   string  `gorm:"email" json:"email"`
+	Email   string  `gorm:"email;unique" json:"email"`
 	Forname string  `gorm:"forename" json:"forename"`
 	Surname string  `gorm:"surname" json:"surname"`
 	Scales  []Scale `json:"scales"`
