d9872e5d5c670ec32aa8c960c5c341997f20ceed
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-09-22 14:42:26 +0100

    setup auth validation on action

--- back_end/go_gin/models/action.go
+++ back_end/go_gin/models/action.go
@@ -17,10 +17,23 @@ type Action struct {
 	Timespans  []Timespan `json:"timespans"`
 }
 
-func (a *Action) Validate() error {
+func (a *Action) Validate(authUser User, db gorm.DB, isCreating bool) error {
 
 	if a.Name == "" {
 		return errors.New("action name is required")
+	}
+
+	return nil
+}
+
+func (a *Action) ValidateAuthorisation(authUser User, db gorm.DB) error {
+
+	// We need to determine the user id that's actually stored against this
+	var actualUserId int64
+	db.Model(&Action{}).Select("`scales`.`user_id`").Joins("JOIN `categories` ON `categories`.`id` = `actions`.`category_id`").Joins("JOIN `scales` ON `scales`.`id` = `categories`.`scale_id`").Where("actions.id = ?", a.ID).First(&actualUserId)
+
+	if uint64(actualUserId) != authUser.ID {
+		return errors.New("user is not authorised to change this action")
 	}
 
 	return nil
--- back_end/go_gin/models/action_test.go
+++ back_end/go_gin/models/action_test.go
@@ -1,12 +1,47 @@
 package models_test
 
 import (
+	"regexp"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/M-Derbyshire/LifeScale/tree/main/back_end/go_gin/models"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func TestActionValidationChecksName(t *testing.T) {
+type ActionSuite struct {
+	suite.Suite
+	DB   *gorm.DB
+	Mock sqlmock.Sqlmock
+}
+
+func (s *ActionSuite) SetupTest() {
+	sqlMockDb, databaseMockExpectations, _ := sqlmock.New()
+
+	var mockDialector = mysql.New(mysql.Config{
+		Conn:                      sqlMockDb,
+		DriverName:                "mysql",
+		SkipInitializeWithVersion: true,
+	})
+	mockDB, _ := gorm.Open(mockDialector, &gorm.Config{})
+
+	s.Mock = databaseMockExpectations
+	s.DB = mockDB
+}
+
+func TestActionSuite(t *testing.T) {
+	suite.Run(t, new(ActionSuite))
+}
+
+func setupActionAuthValidationDbQueryExpect(s *ActionSuite, actionID uint64, expectedUserId uint64) {
+	s.Mock.ExpectQuery(regexp.QuoteMeta("SELECT `scales`.`user_id` FROM `actions` JOIN `categories` ON `categories`.`id` = `actions`.`category_id` JOIN `scales` ON `scales`.`id` = `categories`.`scale_id` WHERE actions.id = ? AND `actions`.`deleted_at` IS NULL ORDER BY `actions`.`id` LIMIT 1")).WithArgs(actionID).WillReturnRows(sqlmock.NewRows([]string{"user_id"}).AddRow(expectedUserId))
+}
+
+// Validation
+
+func (s *ActionSuite) TestActionValidationChecksName() {
 
 	subTests := []struct {
 		TestName  string
@@ -26,7 +61,7 @@ func TestActionValidationChecksName(t *testing.T) {
 	}
 
 	for _, subtest := range subTests {
-		t.Run(subtest.TestName, func(t *testing.T) {
+		s.T().Run(subtest.TestName, func(t *testing.T) {
 
 			action := models.Action{
 				Name:      subtest.Name,
@@ -34,7 +69,7 @@ func TestActionValidationChecksName(t *testing.T) {
 				Timespans: []models.Timespan{},
 			}
 
-			err := action.Validate()
+			err := action.Validate(authUserExample, *s.DB, true)
 
 			if err != nil && !subtest.ExpectErr {
 				t.Errorf("didn't expect a validation error, but recieved: %s", err.Error())
@@ -44,4 +79,64 @@ func TestActionValidationChecksName(t *testing.T) {
 
 		})
 	}
+}
+
+// Auth validation
+
+func (s *ActionSuite) TestActionAuthValidationChecksAuthId() {
+
+	subTests := []struct {
+		TestName     string
+		ActionId     uint64
+		AuthUserId   uint64
+		ActionUserId uint64
+		ExpectErr    bool
+	}{
+		{
+			TestName:     "Matching auth user",
+			ActionId:     1,
+			AuthUserId:   1,
+			ActionUserId: 1,
+			ExpectErr:    false,
+		},
+		{
+			TestName:     "Different auth user",
+			ActionId:     1,
+			AuthUserId:   2,
+			ActionUserId: 1,
+			ExpectErr:    true,
+		},
+	}
+
+	for _, subtest := range subTests {
+		s.T().Run(subtest.TestName, func(t *testing.T) {
+
+			authUser := models.User{
+				ID:       subtest.AuthUserId,
+				Email:    "test@test.com",
+				Forename: "test",
+				Surname:  "test",
+				Scales:   []models.Scale{},
+			}
+
+			action := models.Action{
+				ID:        subtest.ActionId,
+				Name:      "test",
+				Weight:    1,
+				Timespans: []models.Timespan{},
+			}
+
+			setupActionAuthValidationDbQueryExpect(s, subtest.ActionId, subtest.ActionUserId)
+
+			err := action.ValidateAuthorisation(authUser, *s.DB)
+
+			if err != nil && !subtest.ExpectErr {
+				t.Errorf("didn't expect a validation error, but recieved: %s", err.Error())
+			} else if err == nil && subtest.ExpectErr {
+				t.Errorf("expected a validation error, but recieved none")
+			}
+
+		})
+	}
+
 }
