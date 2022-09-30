961188a6432fa18ea3e6c2ac66f3da68af2b5564
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-09-30 15:05:09 +0100

    added jwt info and path prefix to env

--- back_end/go_gin/env/env.go
+++ back_end/go_gin/env/env.go
@@ -9,9 +9,12 @@ import (
 
 // EnvVars holds the environment variables that are stored in the .env file
 type EnvVars struct {
-	DatabaseString string `env:"DATABASE_STRING,required"` // The string used to connect to the database
-	Host           string `env:"HOST,required"`            // The hostname of this application
-	Port           string `env:"PORT,required"`            // The port that this application should use
+	DatabaseString    string `env:"DATABASE_STRING,required"`         // The string used to connect to the database
+	Host              string `env:"HOST,required"`                    // The hostname of this application
+	Port              string `env:"PORT,required"`                    // The port that this application should use
+	PathPrefix        string `env:"PATH_PREFIX,required"`             //The start of all route urls (e.g. "/api/v1" in "/api/v1/user/register")
+	JwtKey            string `env:"JWT_KEY,required"`                 //The key used when generating JWTs
+	JwtExpirationMins int    `env:"JWT_EXPIRATION_TIME_MIN,required"` //The minutes until a JWT will expire
 }
 
 // Loads the .env file variables into an EnvVars object
