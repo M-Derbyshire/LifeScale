127b4330b246f7c007da78b4a490d9fadf78f7bb
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-12-03 14:06:56 +0000

    corrected a compile error in one of the create scale tests

--- back_end/go_gin/services/scaleservice_test.go
+++ back_end/go_gin/services/scaleservice_test.go
@@ -303,7 +303,7 @@ func (s *ScaleServiceSuite) TestCreateReturnsErrorFromDatabase() {
 	expectedScale.StrID = strScaleId
 	expectedScale.ID = scaleId
 
-	service.Create(newScale, 1)        //creating with explicit ID (models stop this from happening through the json at request-time)
+	service.Create(newScale)           //creating with explicit ID (models stop this from happening through the json at request-time)
 	_, err := service.Create(newScale) //creating again, with same ID, to cause error
 	require.Error(s.T(), err)
 }
